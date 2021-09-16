ec7b3502b28eda8876f42a26df702942f0deb783
Ngoc Nguyen <ngocnvtech@gmail.com>
2021-09-16 23:16:47 +0700

    update

--- src/main/java/com/nvgroupitech/truelove/config/JsonConfig.java
+++ src/main/java/com/nvgroupitech/truelove/config/JsonConfig.java
@@ -1,26 +1,26 @@
 package com.nvgroupitech.truelove.config;
 
-import org.springframework.context.annotation.Bean;
+import java.util.List;
+
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
 
 
 @Configuration
-public class JsonConfig {
+public class JsonConfig extends WebMvcConfigurationSupport{
 	
-	@Bean
-	public ObjectMapper objectMapper() {
-		ObjectMapper mapper = new ObjectMapper();
-		SimpleModule module = new SimpleModule("JsonModule");
-				
-		mapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-		mapper.setSerializationInclusion(JsonInclude.Include.NON_EMPTY);
-		mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
-		mapper.registerModule(module);
-		return mapper;
-	}
+	  @Override
+	    protected void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+	        MappingJackson2HttpMessageConverter converter = new MappingJackson2HttpMessageConverter();
+	        ObjectMapper mapper = new ObjectMapper();
+	        mapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
+	        converter.setObjectMapper(mapper);
+	        converters.add(converter);
+	        super.configureMessageConverters(converters);
+	    }
 }
